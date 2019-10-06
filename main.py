92446bcbd0153ffda9e259c4bf9fc070cc65bca4
alex marriott <alex-marriott@live.co.uk>
2019-10-06 19:50:23 +0100

    Removing old template files and added some google templates for the design of the site.
    Started working on the jinja templating for the html.

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    return render_template('index.html')
 
-    return render_template('index.html', times=dummy_times)
+@app.route('/index.html')
+def index():
+    return root()
 
+@app.route('/blog.html')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
