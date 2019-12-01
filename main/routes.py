89f0f954cf3967eee50613c53e9164f45b932d61
Alex Marriott <alex-marriott@live.co.uk>
2019-12-01 11:50:36 +0000

    Slack now works and the messages can be sent to and from correctly. Next i need to add some jquery to poll and update the page with new messages.

--- main/routes.py
+++ main/routes.py
@@ -1,6 +1,8 @@
+import json
+
 import requests
 from flask import Blueprint, render_template, redirect, url_for,request
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from .forms import SlackForm, EmailForm
 main = Blueprint('main', __name__)
@@ -14,7 +16,8 @@ def index():
     return redirect(url_for('main.root'))
 
 @main.route('/contact', methods=['POST', 'GET'])
-def contact(email=None, message=None):
+@login_required
+def contact():
     form = SlackForm()
     #xoxp-847971877056-847971877792-847997585669-c8a17eca7c3853fa0fa4b558461d5774 <- access token to slack
     if form.validate_on_submit():
@@ -28,14 +31,18 @@ def contact(email=None, message=None):
                 #post to slack
                 #curl -X POST -H 'Content-type: application/json'
                 #--data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
-                print(message)
-                resp = requests.post('https://hooks.slack.com/services/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', json={'text':data['message']})
+                resp = requests.post('https://hooks.slack.com/services/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', json={'text' : '{0} @ {1} says: {2}'.format(current_user.name, current_user.email, data['message'])})
                 print(resp)
                 print(resp.status_code)
                 print(resp.text)
             else:
                 pass
-    return render_template('contact.html',action='main.contact', form=form)
+    # Reads all the messages in the website-chat channel
+    channel_data = json.loads(requests.get('https://slack.com/api/channels.history?token=xoxp-847971877056-847971877792-847997585669-c8a17eca7c3853fa0fa4b558461d5774&channel=CQLEU7DMZ&pretty=1').text)
+    channel_messages = []
+    for i in channel_data['messages']:
+        channel_messages.append(i['text'])
+    return render_template('contact.html',action='main.contact', form=form, slack_messages=channel_messages)
 
 @main.route('/about')
 def about():
