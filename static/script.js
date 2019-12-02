97ce279e124cd5d702afcc83fcd8f55b7e30f9e9
AlexMarriott <alex-marriott@live.co.uk>
2019-12-02 11:10:18 +0000

    added a limit to the amount of results being returned, need to test this on a fresh instance as it appears the js has been cached

--- static/script.js
+++ static/script.js
@@ -1,18 +1,22 @@
-    const interval = setInterval(function() {
-        updateList();
-    }, 5000);
+const interval = setInterval(function () {
+    updateList();
+}, 5000);
 
-    function updateList(){
-      var url = 'https://slack.com/api/channels.history?token=xoxp-847971877056-847971877792-856975052375-2afbe577916e8940dc0e4b3bf3f4f3c6&channel=CQLEU7DMZ&pretty=1';
-      var request = new XMLHttpRequest();
-        request.open('GET', url);
-        request.send();
+function updateList() {
+    //10000 = 10 seconds in millseconds
+    // print('It'.concat(' is',' a',' great',' day.'));
+    var timestamp = Date.now() - 10000;
+    var url = 'https://slack.com/api/channels.history?token=xoxp-847971877056-847971877792-856975052375-2afbe577916e8940dc0e4b3bf3f4f3c6&channel=CQLEU7DMZ&count=1&pretty=1';
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.send();
 
-        request.onreadystatechange  = function(){
-          if (this.readyState==4 && this.status==200){
-            console.log(request.responseText);
-          }
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //console.log(request.responseText);
+            console.log(timestamp);
+        }
 
-        };
+    };
 
-    }
+}
