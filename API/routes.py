ca146a45f4972bb24fbc7acbd25d8a454dfa6cda
AlexMarriott <alex-marriott@live.co.uk>
2019-12-06 13:14:11 +0000

    Nearly Done need to complete the adding card.

--- API/routes.py
+++ API/routes.py
@@ -8,18 +8,22 @@ client = trello.TrelloClient(
     api_key=os.environ['trello_api_key'],
     api_secret=os.environ['trello_secret']
 )
-#board = trello..get_board('5de9f7c17cf48e47b2e8302d')
-#trello_list = client.('5de9f7c1ba72a567a9bb40d8')
 
 api = Blueprint('api', __name__)
 
-@api.route('/trello')
-def trello():
+
+@api.route('/trello', methods=['POST', 'GET'])
+def create_trello_card():
     trello_form = TrelloForm()
     if trello_form.validate_on_submit():
         data = request.form.to_dict(flat=True)
+        print(data)
         if [data['card_title'], data['description']] is not None:
             #TODO use the current logged in user.
-            card = Card(data['title'], data['label'] or '', data['description'], 'test_user')
-            #client.
-            return render_template('trello.html', action='api.trello', trello_form=trello_form)
+            trello_list = client.get_list('5de9f7c1ba72a567a9bb40d8')
+            card = Card(data['card_title'], data['label'] or '', data['description'], 'test_user')
+            resp = trello_list.add_card(name=card.title, desc='{0} \nRequested by: {1}'.format(card.description,card.requesting_user, label=card.label))
+            print(resp.labels)
+            return render_template('trello.html', action='api.create_trello_card', trello_form=trello_form)
+
+    return render_template('trello.html', action='api.create_trello_card', trello_form=trello_form)
